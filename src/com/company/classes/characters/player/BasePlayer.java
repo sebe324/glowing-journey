20a74e2890bf2a804a902cd2525fa29f6d0e5b52
sebe324 <sebeq05@gmail.com>
2022-04-02 22:44:30 +0200

    also added basePlayer class

---
+++ src/com/company/classes/characters/player/BasePlayer.java
@@ -0,0 +1,52 @@
+package com.company.classes.characters.player;
+
+import com.company.classes.characters.BaseCharacter;
+
+public class BasePlayer extends BaseCharacter {
+    private int upKey,downKey,leftKey,rightKey, leftAttackKey, rightAttackKey;
+
+    public BasePlayer(int x, int y, String name, int upKey, int downKey, int leftKey, int rightKey, int leftAttackKey, int rightAttackKey) {
+        super(x, y, name);
+        this.upKey = upKey;
+        this.downKey = downKey;
+        this.leftKey = leftKey;
+        this.rightKey = rightKey;
+        this.leftAttackKey = leftAttackKey;
+        this.rightAttackKey = rightAttackKey;
+    }
+
+    public BasePlayer() {
+    }
+
+    public int getUpKey() {
+        return upKey;
+    }
+
+
+    public int getDownKey() {
+        return downKey;
+    }
+
+
+    public int getLeftKey() {
+        return leftKey;
+    }
+
+
+
+    public int getRightKey() {
+        return rightKey;
+    }
+
+
+    public int getLeftAttackKey() {
+        return leftAttackKey;
+    }
+
+
+
+    public int getRightAttackKey() {
+        return rightAttackKey;
+    }
+
+}
